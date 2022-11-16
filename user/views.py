19c9c0c23f2755d458bb1855fea57406b12e83d3
gyuchan <schk9611@gmail.com>
2022-11-16 22:36:23 +0900

    feat(user.urls): 회원가입 urls 작성

--- user/views.py
+++ user/views.py
@@ -1,10 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-from .models import User
 from .serializers import SignupSerializer
 
 
