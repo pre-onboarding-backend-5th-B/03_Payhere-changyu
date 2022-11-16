d65956a97dcb58ae986ea2a3b854dbd259ce2963
gyuchan <schk9611@gmail.com>
2022-11-16 23:39:57 +0900

    feat(account_book.models): 가계부 모델링 작성

--- account_book/models.py
+++ account_book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from user.models import User
+
+
+class AccountBook(models.Model):
+    class StatusChoices(models.TextChoices):
+        INCOME = "I", "수입"
+        EXPENDITURE = "E", "지출"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField(validators=[MinValueValidator(0)])
+    memo = models.CharField(max_length=255)
+    status = models.CharField(max_length=1, choices=StatusChoices.choices, blank=True)
+    is_delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "account_book"
