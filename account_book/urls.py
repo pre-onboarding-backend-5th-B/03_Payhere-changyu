55954233d2155989579b1cbfaeb8e18ca8a65ffb
gyuchan <schk9611@gmail.com>
2022-11-18 10:21:25 +0900

    feat: viewset으로 accountbook 불러오기, 생성하기, 삭제하기 구현

--- account_book/urls.py
+++ account_book/urls.py
@@ -1,17 +1,10 @@
-# from django.urls import path, include
-# from rest_framework import routers
-# from account_book.views import AccountBookViewSet
+from django.urls import path, include
+from rest_framework import routers
+from account_book.views import AccountBookViewSet
 
-# router = routers.DefaultRouter()
-# router.register("", AccountBookViewSet)
-
-# urlpatterns = [
-#     path("", include(router.urls)),
-# ]
-
-from django.urls import path
-from account_book import views
+router = routers.DefaultRouter()
+router.register("", AccountBookViewSet)
 
 urlpatterns = [
-    path("", views.AccountBookListAPIView.as_view(), name="list_view"),
+    path("", include(router.urls)),
 ]
--- account_book/views.py
+++ account_book/views.py
@@ -1,37 +1,22 @@
-# from rest_framework import viewsets, status
-# from rest_framework.response import Response
-# from rest_framework.generics import get_object_or_404
-# from .models import AccountBook
-# from .serializers import AccountBookSerializer
-
-
-# class AccountBookViewSet(viewsets.ModelViewSet):
-#     serializer_class = AccountBookSerializer
-#     queryset = AccountBook.objects.all()
-
-#     def get_queryset(self):
-#         return self.queryset.filter(user=self.request.user.pk)
-
-#     def perform_create(self, serializer):
-#         account_book = get_object_or_404(AccountBook, user=self.request.user.pk)
-#         serializer.save(account_book=account_book)
-#         return super().perform_create(serializer)
-
-#     def perform_destroy(self, instance):
-#         instance.is_delete = True
-#         instance.save()
-#         return super().perform_destroy(instance)
-
-from rest_framework.generics import ListAPIView
-
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from .models import AccountBook
 from .serializers import AccountBookSerializer
 
-from .models import AccountBook
 
-
-class AccountBookListAPIView(ListAPIView):
-    queryset = AccountBook.objects.all()
+class AccountBookViewSet(viewsets.ModelViewSet):
     serializer_class = AccountBookSerializer
+    queryset = AccountBook.objects.all()
 
-    def get(self, request):
-        return self.queryset.filter(user=request.user.pk)
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user.pk)
+
+    def perform_create(self, serializer):
+        account_book = get_object_or_404(AccountBook, user=self.request.user.pk)
+        serializer.save(account_book=account_book)
+        return super().perform_create(serializer)
+
+    def perform_destroy(self, instance):
+        instance.is_delete = True
+        instance.save()
+        return super().perform_destroy(instance)
